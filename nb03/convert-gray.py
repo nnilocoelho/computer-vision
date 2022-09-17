f69c4e4d643c8d552babf9c29c66bb7712447be0
Nilo Coelho <nnilocoelho@gmail.com>
2022-09-17 14:08:11 -0300

    video gray

--- nb03/convert-gray.py
+++ nb03/convert-gray.py
@@ -1,14 +1,14 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('nb03/video-gray-v2.avi')
 
-while(True):
+while(cap.isOpened()):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
--- nb03/video_writing_v2.py
+++ nb03/video_writing_v2.py
@@ -1,8 +1,5 @@
 # import the library
 import cv2
-import matplotlib.pyplot as plt
-
-
 
 source = 0
 cap = cv2.VideoCapture(source)
